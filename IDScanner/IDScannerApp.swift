b4824a4b180501cde87fc167462b730556670842
Gleb <kovgleb04@gmail.com>
2024-09-18 13:33:55 +0300

    Initial Commit

---
+++ IDScanner/IDScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDScannerApp.swift
+//  IDScanner
+//
+//  Created by Kovalev Gleb on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct IDScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
